6beae0232b6f1ace72e34a2460a091eb81847e1d
Arpan Chatterjee <arpanchatterjeeg@gmail.com>
2023-08-19 09:59:15 -0400

    functinal tictac toe game, board not being updated though

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 class TicTacToe:
     def __init__(self) -> None:
         print("New game instance")
-        self.board = [["-","-","-"] for i in range(3)]
+        self.board = [["-","-","-"],["-","-","-"],["-","-","-"]]
         self.p1 = "X"
         self.p2 = "O"
+        self.turns =0
+        self.pos = "00"
 
     def isWon(self) -> bool:
         for i in self.board:
@@ -17,11 +19,26 @@ class TicTacToe:
         if not (self.board[0][2]!="-" and self.board[0][2]==self.board[1][1] and self.board[1][1]==self.board[2][0]):
             return False
         return True
-
+    
+    def insertToken(self, player):
+        x,y = int(self.pos[0]), int(self.pos[1])
+        print(x,y)
+        if player==1:
+            self.board[x][y] == self.p1
+        else:
+            self.board[x][y] == self.p2
+        print(self.board)
+        
     def start(self):
-        while not self.isWon():
-            pass
-
+        while not self.isWon() and self.turns<9:
+            if self.turns%2==0:
+                self.pos = input("Player 1's turn. select your spot e.g [00,12,21,12]")
+                self.insertToken(1)
+            else:
+                self.pos = input("Player 2's turn. select your spot e.g [00,12,21,12]")
+                self.insertToken(2)
+            self.turns+=1
+            self.display()
 
     def display(self):
          print(" ".join(self.board[0]) + "\n" + " ".join(self.board[1]) + "\n" + " ".join(self.board[2]) + "\n")
@@ -31,6 +48,6 @@ class TicTacToe:
 def main():
     game = TicTacToe()
     game.display()
-    print(game)
+    game.start()
 
 main()
